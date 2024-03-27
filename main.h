25b01fed008cf7c174835cf48fc0cafec2c64f0e
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-27 10:24:34 +0100

    betty style + add prototypes

--- main.h
+++ main.h
@@ -1,1 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
+int printf_char(va_list args);
+int printf_nbr(va_list args);
+int printf_percent(va_list args);
+int printf_string(va_list args);
+
+#endif
