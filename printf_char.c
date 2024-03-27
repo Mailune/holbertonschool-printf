0e9f691fefbad8c7050e76a64b2e368f340a0b70
Mailune <mailysrosereau@yahoo.fr>
2024-03-27 12:36:40 +0100

    creation of a new function called printf char

---
+++ printf_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * printf_char - prints a character
+ * @args: list of arguments from variadic function printf
+ * Return: 1, length of the printed character)
+ */
+int printf_char(va_list args)
+{
+	char character = va_arg(args, int);
+
+	_putchar(character);
+	return (1);
+}
