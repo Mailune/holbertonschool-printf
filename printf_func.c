200cfd2fdbde93468e05d3a4552ad7a2a4813291
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-27 11:18:38 +0100

    add specifier task1 + add loop to use the functions

--- printf_func.c
+++ printf_func.c
@@ -3,20 +3,27 @@ include "main.h"
 /**
  * printf_func - check and verif the specifier to send it
  * to the right function to use
- *
- * @s: The specifier
- *
- * Return:
+ * @ptr_specifier: pointer of element
+ * Return: function associate or NULL if not found
  */
-
 
 int (*printf_func(char *ptr_specifier))(va_list)
 {
-	print spe[] = {
+	specifier function[] = {
 		{"c", printf_char},
 		{"s", printf_string},
 		{"%", printf_percent},
+		{"d", printf_nbr},
+		{"i", printf_nbr},
 		{NULL, NULL}
 		};
-	int i = 0;
+
+	int i;
+
+	for (i = 0; function[i].format != NULL; i++)
+	{
+		if (*ptr_specifier == function[i].format)
+			return (function[i].format)
+	}
+	return (NULL);
 }
