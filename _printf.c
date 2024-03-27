adf66a988c9d1e348edf10e4b321be8b259c498d
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-27 16:09:46 +0100

    v1 _printf a lot of error on compilation

--- _printf.c
+++ _printf.c
@@ -8,3 +8,38 @@
 
 int _printf(const char *format, ...)
 {
+	int count = 0;
+	int (*search_func)(va_list);
+	va_list listargs;
+
+	va_start(listargs, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++;
+			search_func = printf_func(format);
+
+			if (search_func != NULL)
+				count = count + search_func(list_args);
+			else
+			{
+				_putchar('%');
+				_putchar(*format);
+				count = count + 2;
+			}
+		}
+		format++;
+	}
+	va_end(listargs);
+	return (count);
+}
