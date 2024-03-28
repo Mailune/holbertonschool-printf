dd11897d5bfdab1782e347964e793d1540e5ec94
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-28 14:34:47 +0100

    betty doc

--- main.h
+++ main.h
@@ -6,16 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int (*printf_func(const char *ptr_specifier))(va_list);
-int _printf(const char *format, ...);
-
-int _putchar(char c);
-int printf_char(va_list args);
-int printf_nbr(va_list args);
-int printf_percent(va_list args);
-int printf_string(va_list args);
-
-/*
+/**
  * struct specifier - represent a format specifier and its associated function
  * @format: the format specifier
  * @function: function of the format specified
@@ -26,5 +17,14 @@ typedef struct specifier
 	char format;
 	int (*function)(va_list);
 } specif;
+
+int (*printf_func(const char *ptr_specifier))(va_list);
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+int printf_char(va_list args);
+int printf_nbr(va_list args);
+int printf_percent(va_list args);
+int printf_string(va_list args);
 
 #endif
