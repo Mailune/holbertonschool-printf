d25364b43b44cbb2b47f1b8b4a953a2240a1dc7f
Mailune <mailysrosereau@yahoo.fr>
2024-03-29 14:34:26 +0100

    add comments on the code

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
  *
  * Return: Always 0
  */
-int main(void)
+int main(void)	/* Defines main function. */
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
+	int len;	/* Stores lengths of printed output. */
+	int len2;	/* Stores lengths of printed output. */
+	unsigned int ui;	/* Stores unsigned integer value. */
+	void *addr;	/* Stores memory address. */
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
+	len = _printf("Let's try to printf a simple sentence.\n");/* Custom printf*/
+	len2 = printf("Let's try to printf a simple sentence.\n");/* Standard printf*/
+	ui = (unsigned int)INT_MAX + 1024;	/* Calculates unsigned int value. */
+	addr = (void *)0x7ffe637541f0;	/* Assigns memory address. */
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 
 	printf("\"(Ôo)'\n");
 	_printf("\"(Ôo)'\n");
-	
+
 
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
