e32b9cc672e00b42768730a0d9070bc57a134237
Mailune <mailysrosereau@yahoo.fr>
2024-03-29 14:37:06 +0100

    add comments on the code

--- printf_char.c
+++ printf_char.c
@@ -5,10 +5,10 @@
  * @args: the list of arguments from variadic function printf
  * Return: 1, length of the printed character)
  */
-int printf_char(va_list args)
+int printf_char(va_list args) /* func printf_char taking va_list argument */
 {
-	char character = va_arg(args, int);
+	char character = va_arg(args, int);	/* Retrieves next va_list arg as int */
 
-	_putchar(character);
-	return (1);
+	_putchar(character);	/* Prints character using _putchar. */
+	return (1);	/* Returns 1 for single character printed. */
 }
