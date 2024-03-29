27f6108121f31971db6fece573b393d54be796b9
Mailune <mailysrosereau@yahoo.fr>
2024-03-29 14:33:38 +0100

    add comments on the code

--- _putchar.c
+++ _putchar.c
@@ -8,7 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char c)	/* Defines the _putchar func with character parameter */
 {
-	return (write(1, &c, 1));
+	return (write(1, &c, 1));	/* Writes character to stdout using write func */
 }
